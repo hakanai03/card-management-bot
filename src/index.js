8f81a911e845268e65bfa3bb110d2f4665bbd6e1
Shogo Ishimaru <hakanai.blog@gmail.com>
2019-03-13 12:10:37 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-const 
+const Discord = require("discord.js")
+
+if(!process.env.DISCORD_USER_TOKEN) throw Error("Undefined discord user token")
+
+const client = new Discord.Client()
+const token = process.env.DISCORD_USER_TOKEN
+
+const onMessage = async(message) => {
+  if(message.author.bot)return
+  const res = await message.reply(message.content)
+  console.log(`Sent message: ${res}`)
+  return
+}
+
+client.on("ready", () => console.log("listen"))
+client.on("message", onMessage)
+client.login(token)
